c991b4dbe15314a201f004cc00d0d640126ba671
Kevin Roark <ker2143@columbia.edu>
2014-02-28 14:43:00 -0500

    started xcode project

---
+++ Network Style/Network Style/NetStyleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NetStyleAppDelegate.h
+//  Network Style
+//
+//  Created by Kevin Roark on 2/28/14.
+//  Copyright (c) 2014 Miller's Fantasy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NetStyleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Network Style/Network Style/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Network Style
+//
+//  Created by Kevin Roark on 2/28/14.
+//  Copyright (c) 2014 Miller's Fantasy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NetStyleAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NetStyleAppDelegate class]));
+    }
+}
